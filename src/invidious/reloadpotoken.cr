6240405ada95e64832c1d995f469933cc4a04be8
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-14 17:11:42 -0400

    fixed youtube_api.cr?

--- src/invidious/reloadpotoken.cr
+++ src/invidious/reloadpotoken.cr
@@ -28,7 +28,7 @@ class ReloadPOToken
 
     config = Config.from_yaml(config_yaml)
     
-    LOGGER.info("reloadpottoken.cr: get_tokens: pot: " + @@pot)
+    LOGGER.info("reloadpottoken.cr: get_tokens: pot: " + @@pot.as(String))
     LOGGER.info("reloadpottoken.cr: get_tokens: vdata: " + @@vdata)
 
     @@pot = config.po_token
